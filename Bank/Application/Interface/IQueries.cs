2fa5e33c508d9a34a93fae3db8f9599b9b4a1664
Juesdamo1214 <juesdamo1412@gmail.com>
2023-11-10 12:07:48 -0500

    feat(SB-Queries)Create queries

---
+++ Bank/Application/Interface/IQueries.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+
+namespace Application.Interface
+{
+    public interface IQueries
+    {
+        IEnumerable<BankAccount> GetAllAccounts();
+        BankAccount GetByIdAccount(Guid id);
+        IEnumerable<Transaction> GetAllTransactions();
+        Transaction GetByIdTransaction(Guid id);
+        IEnumerable<Transaction> TransactionAccountGetById(Guid id);
+    }
+}
---
+++ Bank/Application/Services/Queries/GetByIdTransactionAccount.cs
@@ -0,0 +1,20 @@
+﻿using Application.Context;
+using Domain.Models;
+
+namespace Application.Services.Queries
+{
+    public class GetByIdTransactionAccount
+    {
+        BankContext context;
+        
+        public GetByIdTransactionAccount(BankContext dbcontext)
+        {
+            context = dbcontext;
+        }
+        public IEnumerable<Transaction> TransactionAccountGetById(Guid id)
+        {
+            var listTransaction = context.Transactions.Where(item => item.IdAccount == id);
+            return listTransaction.ToList();
+        }
+    }
+}
